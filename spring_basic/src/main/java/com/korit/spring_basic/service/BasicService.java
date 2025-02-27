9b7e408c62f41a9619653cdca2674c77abf8b07e
OJaes <ojg1117@gmail.com>
2025-02-27 17:20:23 +0900

    0227 spring service, cors

---
+++ spring_basic/src/main/java/com/korit/spring_basic/service/BasicService.java
@@ -0,0 +1,10 @@
+package com.korit.spring_basic.service;
+
+import org.springframework.http.ResponseEntity;
+
+public interface BasicService {
+    
+    ResponseEntity<String> getService();
+
+    
+}
